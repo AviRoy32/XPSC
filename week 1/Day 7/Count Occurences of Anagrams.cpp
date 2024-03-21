246ea8e864ca640836dfc7036641f935b7348896
AviRoy32 <163465894+AviRoy32@users.noreply.github.com>
2024-03-21 21:59:56 +0600

    Add files via upload
    Added day 7 codes

---
+++ week 1/Day 7/Count Occurences of Anagrams.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+    int search(string pat, string txt) {
+        // code here
+        vector<int>frq1(26,0);
+        vector<int>frq2(26,0);
+        for(char c: pat)
+        {
+            frq1[c-'a']++;
+        }
+        int ans=0;
+        int r=0,l=0;
+        while(r<txt.size())
+        {
+            frq2[txt[r]-'a']++;
+            if(r-l+1==pat.size())
+            {
+                if(frq1==frq2)
+                {
+                    ans++;
+                }
+                frq2[txt[l]-'a']-=1;
+                l++;
+                r++;
+            }
+            else r++;
+        }
+        return ans;
+
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string pat, txt;
+        cin >> txt >> pat;
+        Solution ob;
+        auto ans = ob.search(pat, txt);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
